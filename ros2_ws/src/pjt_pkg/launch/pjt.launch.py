8274592f08b819ecf07546daf2a1adab97d553dd
YooJuno <juno980220@naver.com>
2025-05-22 19:32:29 +0900

    launch파일 수정 및 setup

---
+++ ros2_ws/src/pjt_pkg/launch/pjt.launch.py
@@ -0,0 +1,53 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(
+                    get_package_share_directory('realsense2_camera'),
+                    'launch',
+                    'rs_launch.py'
+                )
+            )
+        ),
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource(
+        #         os.path.join(
+        #             get_package_share_directory('dobot_bringup'),
+        #             'launch',
+        #             'dobot_magician.launch.py'
+        #         )
+        #     )
+        # ),
+
+        Node(
+            package='dobot_pkg',
+            executable='dobot_target_follower_node',
+            output='screen'
+        ),
+
+        Node(
+            package='vision_pkg',
+            executable='perspective_transformer_node',
+            output='screen'
+        ),
+
+        Node(
+            package='yolo_pkg',
+            executable='yolo_node',
+            output='screen'
+        ),
+
+        Node(
+            package='server_pkg',
+            executable='server_node',
+            output='screen'
+        ),
+    ])
