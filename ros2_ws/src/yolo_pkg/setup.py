2e92934816a76478dfc4b0bf2c8b02c56a3926d5
YooJuno <juno908220@naver.com>
2025-05-22 16:39:10 +0900

    pkg 정리

--- ros2_ws/src/yolo_pkg/setup.py
+++ ros2_ws/src/yolo_pkg/setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
-from glob import glob
-import os
 
-package_name = 'rpi_pkg'
+package_name = 'yolo_pkg'
 
 setup(
     name=package_name,
@@ -12,7 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +20,6 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'conveyor_node = rpi_pkg.conveyor:main'
         ],
     },
 )
