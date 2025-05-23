a2c158b0fd7e05a4ebb1fc333f739676f9285d02
YooJuno <juno908220@naver.com>
2025-05-23 09:41:22 +0900

    ros 정리

--- ros2_ws/src/yolo_pkg/setup.py
+++ ros2_ws/src/yolo_pkg/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch'),glob('launch/*.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,6 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'yolo_node = yolo_pkg.yolo:main'
         ],
     },
 )
