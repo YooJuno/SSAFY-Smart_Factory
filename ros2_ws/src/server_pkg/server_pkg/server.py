58dbaf68bf3e651bc7be70e37f6126708c739118
YooJuno <juno908220@naver.com>
2025-05-23 15:54:36 +0900

    feat: server pub, sub 추가

--- ros2_ws/src/server_pkg/server_pkg/server.py
+++ ros2_ws/src/server_pkg/server_pkg/server.py
@@ -6,6 +6,8 @@ from std_msgs.msg import Bool
 import math
 import time
 from sensor_msgs.msg import Image 
+from std_msgs.msg import String
+from geometry_msgs.msg import Point
 
 def add_offset(pose):
     a_tan = math.atan2(pose[0], pose[1])
@@ -17,43 +19,24 @@ def add_offset(pose):
 
     return pose[0] + offset_x, pose[1] + offset_y, pose[2] - offset_z
 
-class DobotStatusServer(Node):
+class DobotServer(Node):
     def __init__(self):
         super().__init__('dobot_status_server')
 
-        self.create_subscription(
-            JointState,
-            '/dobot_joint_states',
-            self.joint_callback,
-            10
-        )
+        self.create_subscription(JointState, '/dobot_joint_states', self.joint_callback, 10)
+        self.create_subscription(PoseStamped, '/dobot_TCP', self.tcp_callback, 10)
+        self.create_subscription(Bool, '/gripper_status_rviz', self.suction_callback, 10)
+        self.create_subscription(Image, '/coord_space_image', self.image_callback, 10)
+        self.create_subscription(String, '/detection_results', self.yolo_callback, 10)
 
-        self.create_subscription(
-            PoseStamped,
-            '/dobot_TCP',
-            self.tcp_callback,
-            10
-        )
+        self.publisher_target_point = self.create_publisher(Point, '/target_pos', 10)
 
-        self.create_subscription(
-            Bool,
-            '/gripper_status_rviz',
-            self.suction_callback,
-            10
-        )
-
-        self.create_subscription(
-            Image,
-            '/coord_space_image',
-            self.image_callback,
-            10
-        )
 
     def joint_callback(self, msg):
         for name, pos in zip(msg.name, msg.position):
             self.get_logger().info(f'{name}: {math.degrees(pos):.2f} deg')
         print()
-        time.sleep(0.1)
+        time.sleep(0.05)
         '''
         joint1 : -135 ~ 125 (degree)
         joint2 : -5 ~ 40 (degree)
@@ -74,17 +57,23 @@ class DobotStatusServer(Node):
         Z : -130 ~ 113 (mm)
         '''
 
-        time.sleep(0.1)
+        time.sleep(0.05)
 
     def suction_callback(self, msg):
         self.get_logger().info(f'Suction: {"ON" if msg.data else "OFF"}')
+        time.sleep(0.05)
 
     def image_callback(self, msg):
-        pass
+        
+        time.sleep(0.05)
+
+    def yolo_callback(self, msg):
+        
+        time.sleep(0.05)
 
 def main(args=None):
     rclpy.init(args=args)
-    node = DobotStatusServer()
+    node = DobotServer()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
