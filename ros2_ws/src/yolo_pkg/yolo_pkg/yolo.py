2599732422bf9ec2ad6f42447073ff8b75a29362
YooJuno <juno908220@naver.com>
2025-05-27 11:53:45 +0900

    욜로 최신으로 업데이트. 수정 금지

--- ros2_ws/src/yolo_pkg/yolo_pkg/yolo.py
+++ ros2_ws/src/yolo_pkg/yolo_pkg/yolo.py
@@ -11,12 +11,20 @@ from cv_bridge import CvBridge
 
 import torch
 
+H = 0
+S = 0
+V = 0
+count = 0
+
+package_name = 'yolo_pkg'
+
 class ConveyorYoloNode(Node):
     def __init__(self):
         super().__init__('yolo_node')
 
         # YOLOv5 모델 로드 (경로는 상황에 맞게 수정)
-        self.yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', path='/home/ssafy/SSAFY-Smart_Factory/ros2_ws/src/yolo_pkg/model/best.pt')
+        model_path = '/home/ssafy/SSAFY-Smart_Factory/ros2_ws/src/yolo_pkg/model/best.pt'
+        self.yolo_model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
 
         # 이미지 구독
         self.image_sub = self.create_subscription(
@@ -33,13 +41,20 @@ class ConveyorYoloNode(Node):
 
     def get_color_name(self, hsv_color):
         h, s, v = hsv_color
-        if 20 < h < 60 and s < 80 and 120 < v < 255:
-            return 'white'
-        elif ((h < 10 or h > 160) and s > 100 and v > 60):
-            return 'red'
-        elif 80 < h < 150 and s > 80 and v > 40:
+        print(f"[HSV 평균값] H: {h:.2f}, S: {s:.2f}, V: {v:.2f}", end=" - ")
+
+        # BLUE: H값이 100~120, S가 높고 V는 중간 이상
+        if 100 < h < 120 and s > 230 and v > 50:
+            print("blue")
             return 'blue'
-        return 'unknown'
+        # WHITE: S가 매우 낮고 V가 높음
+        elif s < 30 and v > 100:
+            print("white")
+            return 'white'
+        print("red")
+        return 'red'
+
+
 
     def get_color_bgr(self, color_name):
         if color_name == 'white':
@@ -89,6 +104,9 @@ class ConveyorYoloNode(Node):
                 color_image, f'{label}-{color_name}', (x1, y1-10),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, color_bgr, 2
             )
+
+        cv2.imshow('object detection', color_image)
+        cv2.waitKey(1)
 
         self.detection_publisher.publish(detection_result)
         ros_image_message = self.bridge.cv2_to_imgmsg(color_image, encoding='bgr8')
